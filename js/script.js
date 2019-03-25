a5f8b17656746d59332646a05c279b701c1b99b7
Darrion Gering and Gavin Kimberlin <darrion@>
2019-03-25 13:25:23 -0700

    added getNumbers function and created a loop.

--- js/script.js
+++ js/script.js
@@ -14,25 +14,35 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+var getNumbers = function() {
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+  var operation = prompt("Would you like to add, subtract, multiply or divide, or q to quit?");
 
-var operation = prompt("Would you like to add, subtract, multiply or divide?");
+  if(operation === "add") {
+    alert(add(number1, number2));
+    getNumbers();
+  }
+  else if(operation === "subtract") {
+    alert(subtract(number1, number2));
+    getNumbers();
+  }
+  else if(operation === "multiply") {
+    alert(multiply(number1, number2));
+    getNumbers();
+  }
+  else if(operation === "divide") {
+    alert(divide(number1, number2));
+    getNumbers();
+  }
+  else if(operation === "q") {
+    return 0;
+  }
+  else {
+    alert("Invalid input. Try again!");
+    getNumbers();
+  }
+};
 
-if(operation === "add") {
-  alert(add(number1, number2));
-}
-else if(operation === "subtract") {
-  alert(subtract(number1, number2));
-}
-else if(operation === "multiply") {
-  alert(multiply(number1, number2));
-}
-else if(operation === "divide") {
-  alert(divide(number1, number2));
-}
-else {
-  alert("That is not a correct input. Please refresh the page and try again.")
-
-}
+getNumbers();
